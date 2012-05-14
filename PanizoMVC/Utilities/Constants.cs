af12b33d82398e9fe09cbb30401d5f4e3ad0d263
SirMartin <sirmartin@gmail.com>
2012-05-14 21:49:32 +0200

    Creadas constantes para usuarios.

---
+++ PanizoMVC/Utilities/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PanizoMVC.Utilities
+{
+    /// <summary>
+    /// Una clase con todas las constantes necesarias de forma global.
+    /// </summary>
+    public class Constants
+    {
+        /// <summary>
+        /// Separador para las identitys de la aplicación.
+        /// </summary>
+        public const String IdentitySeparator = "-*/*/-";
+    }
+}
